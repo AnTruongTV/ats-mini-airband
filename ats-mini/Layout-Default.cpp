9739d442b32042d056ab5f0112c8531961060415
Nguyễn An Trường <87806359+AnTruongTV@users.noreply.github.com>
2026-09-13 20:43:56 +0700

    Refactor bandwidth text formatting logic

--- ats-mini/Layout-Default.cpp
+++ ats-mini/Layout-Default.cpp
@@ -535,10 +535,10 @@ spr.drawString(bandModeDesc[currentMode], 133, 10, 2);
 // ----- Bandwidth -----
 char bwText[12];
 
-if (strcmp(getCurrentBandwidth()->desc, "Auto") == 0)
-  sprintf(bwText, "F%s", getCurrentBandwidth()->desc);
+if (bandIdx == 2 || bandIdx == 3)
+    snprintf(bwText, sizeof(bwText), "FAuto");
 else
-  sprintf(bwText, "F%sHz", getCurrentBandwidth()->desc);
+    snprintf(bwText, sizeof(bwText), "F%sHz", getCurrentBandwidth()->desc);
 
 spr.setTextColor(TFT_WHITE);
 spr.setTextDatum(MC_DATUM);
